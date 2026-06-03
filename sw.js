cecc87d2ea15398021453933f1828e35903cedd8
DenApp999 <denis.milesi@gmail.com>
2026-06-04 00:08:57 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', () => self.skipWaiting())
+
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()))
+
+self.addEventListener('fetch', (e) => {
+e.respondWith(
+caches.match(e.request).then(cached => cached || fetch(e.request))
+)
+})
